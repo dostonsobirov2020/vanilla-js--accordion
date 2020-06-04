ba39e23ac1982b3492608605e268ebbd5fb6d7dd
dostonsobirov2020 <doston.sobirov.2020@mail.ru>
2020-06-05 02:44:58 +0500

    Accordion using HTML, CSS and Vaniall JS

---
+++ script.js
@@ -0,0 +1,20 @@
+const accordionItemHeaders = document.querySelectorAll('.accordion-item--header');
+
+accordionItemHeaders.forEach(headerItem => {
+    headerItem.addEventListener('click', () => {
+        const activeAccordionItem = document.querySelector('.accordion-item--header.active');
+        if (activeAccordionItem && activeAccordionItem !== headerItem) {
+            activeAccordionItem.classList.remove('active');
+            activeAccordionItem.nextElementSibling.style.maxHeight = '0px';
+        }
+
+        const accordionItemBody = headerItem.nextElementSibling;
+        headerItem.classList.toggle('active');
+        
+        if (headerItem.classList.contains('active')) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = "0px";
+        }
+    });
+});
